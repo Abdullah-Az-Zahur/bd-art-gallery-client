f4eff1257c05f9cbf0ac524d510bc5ef486d4439
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-04-28 11:29:00 +0600

    install helmet

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
 import Root from "../layouts/Root";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
     ],
   },
 ]);
